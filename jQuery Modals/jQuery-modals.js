447b81038cb530aa96a55a253c384f9b3433771c
Jordan Clark <jordan@lumisi.com>
2024-06-24 16:29:17 +0100

    Added new modal boxes

--- jQuery Modals/jQuery-modals.js
+++ jQuery Modals/jQuery-modals.js
@@ -5,36 +5,36 @@
 // Check the attached HTML doc for an example
 
 // Set the timing for the animation. Default is 400ms
-const animationTiming = 400;
+const animationTiming = 150;
 
 // Set this to the class name of all modal tiles
 const tileClass = "tile-class";
 
 // Each unique selector used as both ID's for the buttons and Class's for the tiles
 const selectorNames = [
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-  "ten",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+	"ten",
 ];
 
 jQuery(document).ready(function () {
-  // Hide all tiles on page load
-  jQuery(`.${tileClass}`).hide();
+	// Hide all tiles on page load
+	jQuery(`.${tileClass}`).hide();
 
-  // Function loops through all tiles and applies fadeOut / fadeIn animations
-  selectorNames.forEach((selectorName) => {
-    jQuery(`#${selectorName}`).click(function () {
-      jQuery(`.${tileClass}`).fadeOut(animationTiming);
-      jQuery(`.${selectorName}`).fadeIn(animationTiming);
-    });
-  });
+	// Function loops through all tiles and applies fadeOut / fadeIn animations
+	selectorNames.forEach(selectorName => {
+		jQuery(`#${selectorName}`).click(function () {
+			jQuery(`.${tileClass}`).fadeOut(animationTiming);
+			jQuery(`.${selectorName}`).fadeIn(animationTiming);
+		});
+	});
 });
 
 /* ------------------------------------------------------------------------------------------------ */
@@ -42,17 +42,17 @@ jQuery(document).ready(function () {
 // If you want to show a tile on page load then use this function and add the selector for the tile you want to show
 
 jQuery(document).ready(function () {
-  // Hide all tiles on page load
-  jQuery(`.${tileClass}`).hide();
+	// Hide all tiles on page load
+	jQuery(`.${tileClass}`).hide();
 
-  // Show specific tile on page load
-  jQuery(".example").show(); // <-- add selector here
+	// Show specific tile on page load
+	jQuery(".example").show(); // <-- add selector here
 
-  // Function loops through all tiles and applies fadeOut / fadeIn animations
-  selectorNames.forEach((selectorName) => {
-    jQuery(`#${selectorName}`).click(function () {
-      jQuery(`.${tileClass}`).fadeOut(animationTiming);
-      jQuery(`.${selectorName}`).fadeIn(animationTiming);
-    });
-  });
+	// Function loops through all tiles and applies fadeOut / fadeIn animations
+	selectorNames.forEach(selectorName => {
+		jQuery(`#${selectorName}`).click(function () {
+			jQuery(`.${tileClass}`).fadeOut(animationTiming);
+			jQuery(`.${selectorName}`).fadeIn(animationTiming);
+		});
+	});
 });
